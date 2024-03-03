d1cfdac63c5240ad94928ab7a995725cc791aca8
SofikoSofo <sopobaratashvili@gmail.com>
2024-03-04 01:14:02 +0400

    second commit

--- DAY 0/Lesson 0.py
+++ DAY 0/Lesson 0.py
@@ -90,3 +90,4 @@ end_fill()
 
 
 exitonclick()
+
---
+++ DAY 1/lesson1.py
@@ -0,0 +1,13 @@
+name = "მე ვარ"
+name1 = "სოფო"
+name2 = "ჩემი სიმაღლეა"
+surname = "ბარათაშვილი"
+age = 52
+height = 165.5
+print(name + " "  + name1 + " "  + surname + " "  + str(age) + " "  + name2 + " "  + str(height))
+
+# მოკელე ჩანაწერის ასეთი ფორმა შეცდომა იქნება?
+name3 = "მე ვარ სოფო ბარათაშვილი"
+name4 = "52 წლის"
+name5 = "ჩემი სიმაღლეა 165,5 სმ"
+print(name3 + " "  + name4 + " "  +name5)
