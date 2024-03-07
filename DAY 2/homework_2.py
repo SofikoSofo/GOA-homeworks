c143280e929de93bdb7c860bdc2ec2277c0db19a
SofikoSofo <sopobaratashvili@gmail.com>
2024-03-07 21:40:59 +0400

    third commit

---
+++ DAY 2/homework_2.py
@@ -0,0 +1,7 @@
+# დავალება:  დაპრინტეთ თქვენი აზრი გოაზე,
+
+
+print("COA is a best Academy in Georgia ")
+print("Here are the best mentors and teachers, Deepest thanks to them")
+print("I am lucky to be here")
+print("I will do everything to achieve the goal")
