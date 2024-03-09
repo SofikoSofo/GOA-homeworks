1242027c0ef960d87e22a25e37c1d22c6a451375
SofikoSofo <sopobaratashvili@gmail.com>
2024-03-09 11:58:59 +0400

    sixth commit

---
+++ DAY 5/homevork_5.py
@@ -0,0 +1,33 @@
+# დავალება: შექმენით თქვენი ოჯახის წევრების ასაკებისგან ცვლადები  დაპრინტეთ ასაკები 5 წლის შემდეგ
+
+
+name = ("sofo baratashvili. ")
+age = 52
+print("I em " +  name + "I will be in five years " + str((age) + 5))
+# It's me
+
+name = ("Giorgi Asatiani. ")
+age = 58
+print("My husband is " +  name + " In five years he will be " + str((age) + 5))
+# This is my husband
+
+name = ("Mamuka Asatiani. ")
+age = 30
+print("My son is " +  name + " In five years he will be " + str((age) + 5))
+# This is my son
+
+name = ("Gvantca Gabidauri. ")
+age = 30
+print("My son son's wife is " +  name + " In five years she will be " + str((age) + 5))
+# This is my son's wife
+
+
+name = ("Elene Asatiani. ")
+age = 29
+print("My daughter is " +  name + " In five years she will be " + str((age) + 5))
+# This is my daughter
+
+name = ("Katerina Cagareishvili. ")
+age = 8
+print("My granddaughter is " +  name + " In five years she will be " + str((age) + 5))
+# This is my granddaughter
